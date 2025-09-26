070d3c6641334ab8f9f357f05c4621f0ba0c8abf
jasonliuuuu <jason890418123@gmail.com>
2025-09-26 00:52:07 -0500

    9/25 update

---
+++ 7_Parallel-in, Serial-out Shift Register.sv
@@ -0,0 +1,27 @@
+module model #(parameter
+  DATA_WIDTH = 16
+) (
+  input clk,
+  input resetn,
+  input [DATA_WIDTH-1:0] din,
+  input din_en,
+  output logic dout
+);
+  logic [DATA_WIDTH -1:0] temp; 
+  
+  always @(posedge clk) begin
+    if (!resetn) begin
+      temp <= 0; 
+    end
+    else if (din_en) begin
+      temp <= din; 
+    end
+    else begin
+      temp <= temp >>1; 
+    end
+  end
+
+  
+  assign dout = temp[0];
+
+endmodule
